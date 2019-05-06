f60d99ba3a03481e133edf01ae741283a6145341
parsa alian <parsaalian0@gmail.com>
2019-05-06 22:44:25 +0430

    writing in div added

---
+++ src/components/Page.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import './Page.css';
+
+export default class Page extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { active: false };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleType = this.handleType.bind(this);
+  }
+
+  componentWillMount() {
+    document.addEventListener('mousedown', this.handleClick, false);
+    document.addEventListener('keydown', this.handleType);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.handleClick, false);
+    document.removeEventListener('keydown', this.handleType);
+  }
+
+  handleClick(e) {
+    if (this.node.contains(e.target)) {
+      this.setState({
+        active: true
+      });
+    }
+    else {
+      this.setState({
+        active: false
+      });
+    }
+    console.log(this.state.active);
+  }
+
+  handleType(e) {
+    if (this.state.active) {
+      document.getElementById('type-target').textContent += e.key;
+    }
+  }
+
+  render() {
+    return (
+      <div id='type-target' className='page' ref={node => this.node = node}></div>
+    );
+  }
+}
