117506eb3639978dac048b684d0f0f51c470679a
parsa alian <parsaalian0@gmail.com>
2019-07-08 00:17:29 +0430

    react-scripts fixed and style bug fixed

--- src/app/editor/styles/base/paragraph.jsx
+++ src/app/editor/styles/base/paragraph.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Style from '../../style';
+import Style from '../style';
 
 // eslint-disable-next-line react/prop-types
 export default function Paragraph({ children }) {
