c95d4842245745dd20cf32f2780524af6a1e236c
parsa alian <parsaalian0@gmail.com>
2019-08-20 16:50:57 +0430

    refactor: removed

--- scripts/git/commit.js
+++ scripts/git/commit.js
@@ -5,24 +5,13 @@ const commander = require('commander');
 commander
   .version('1.0.0', '-v, --version')
   .usage('[OPTIONS]...')
-  .option('-ts', '--types', 'Help for commit types')
   .option('-t, --type <type>', 'Type of commit')
   .option('-m, --message <message>', 'Commit message')
   .parse(process.argv);
 
 commander.parse(process.argv);
 
-console.log(commander.types);
-
-if (commander.types) {
-  console.log(' - feat (new feature)');
-  console.log(' - fix (bug fix)');
-  console.log(' - docs (changes to documentation)');
-  console.log(' - style (formatting, missing semi colons, etc; no code change)');
-  console.log(' - refactor (refactoring production code)');
-  console.log(' - test (adding missing tests, refactoring tests; no production code change)');
-  console.log(' - chore (updating grunt tasks etc; no production code change)');
-} else if (!commander.type || !commander.message) {
+if (!commander.type || !commander.message) {
   console.log('You must enter commit type and message.');
 } else {
   const pattern = /(chore|feat|docs|fix|refactor|style|test)(:)( )(.{0,80})/;
