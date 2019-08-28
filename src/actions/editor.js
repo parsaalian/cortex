959b416ae8672f4076feeeca76bd7f10152fa859
parsa alian <parsaalian0@gmail.com>
2019-08-29 00:06:14 +0430

    added style action

--- src/actions/editor.js
+++ src/actions/editor.js
@@ -1,8 +1,16 @@
 // @flow
 import { createAction } from 'redux-act';
+import type { StyleType } from '../types/editor';
 
 // cursor actions
 export const moveCursor = createAction(
   'Move cursor towards given direction (left, right,up, down)',
   (direction: string): { direction: string } => ({ direction }),
 );
+
+// styling actions
+export const changeStyle = createAction('Change the style on select in panel', (style: StyleType): {
+  style: StyleType,
+} => ({
+  style,
+}));
---
+++ src/types/editor.js
@@ -0,0 +1,17 @@
+// @flow
+export type StyleType = {
+  format?: ?{
+    direction?: ?string,
+    text?: {
+      bold?: ?boolean,
+      italic?: ?boolean,
+      underline?: ?boolean,
+      strikethrough?: ?boolean,
+      script?: ?string,
+      case?: ?string,
+    },
+    paragraph?: ?{
+      style?: ?string,
+    },
+  },
+};
