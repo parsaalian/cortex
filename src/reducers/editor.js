105f69215d8d4c901716c048ec69c0a58def086c
parsa alian <parsaalian0@gmail.com>
2019-08-29 15:22:36 +0430

    added reducers

---
+++ src/reducers/editor.js
@@ -0,0 +1,28 @@
+// @flow
+import { createReducer } from 'redux-act';
+import type { StateType } from '../types/state';
+import type { MoveCursorType, ChangeStyleType, TypeCharType } from '../types/editor';
+import { moveCursor, changeStyle, typeChar, removeChar } from '../actions/editor';
+
+// cursor reducers
+export const cursorReducer = createReducer({
+  [moveCursor]: (state: StateType, payload: MoveCursorType): StateType => {
+    const { direction } = payload;
+  },
+});
+
+// style reducers
+export const styleReducer = createReducer({
+  [changeStyle]: (state: StateType, payload: ChangeStyleType): StateType => {
+    const { style } = payload;
+  },
+});
+
+// typing reducers
+export const typingReducer = createReducer({
+  [typeChar]: (state: StateType, payload: TypeCharType): StateType => {
+    const { char } = payload;
+  },
+
+  [removeChar]: (state: StateType): StateType => {},
+});
