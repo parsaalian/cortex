4e60b4710fbfd9dd33bc7f2fcd0c60970bf18c42
parsa alian <parsaalian0@gmail.com>
2019-09-03 00:52:23 +0430

    barely working without styles

---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Head from 'next/head';
+import Editor from '../web/views/editor';
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>Editor</title>
+    </Head>
+    <Editor />
+  </div>
+);
+
+export default Home;
