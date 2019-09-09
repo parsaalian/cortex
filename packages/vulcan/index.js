0e359471ddc06bb905a2834c2407465d67721b29
parsa alian <parsaalian0@gmail.com>
2019-09-10 00:21:16 +0430

    basic gap buffer implemented

--- packages/vulcan/index.js
+++ packages/vulcan/index.js
@@ -3,42 +3,59 @@ import _ from 'lodash';
 const VOID = 'VOID';
 
 export default class GapBuffer {
-  constructor(bufferSize = 128) {
-    this.bufferSize = bufferSize;
-    this.cursor = 0;
-    this.document = _.times(bufferSize, _.constant(VOID));
-    this.lastBufferIndex = 127;
+  constructor(size = 128) {
+    this.size = size;
+    this.gapSize = size;
+    this.gapLeft = 0;
+    this.gapRight = 127;
+    this.document = _.times(size, _.constant(VOID));
   }
 
   grow(position) {
     this.document = [
       ..._.slice(this.document, 0, position),
-      ..._.times(this.bufferSize, _.constant(VOID)),
+      ..._.times(this.size, _.constant(VOID)),
       ..._.slice(this.document, position),
     ];
-    this.lastBufferIndex = position + this.bufferSize - 1;
+    this.gapLeft = position;
+    this.gapRight = position + this.size - 1;
+    this.gapSize = this.size;
   }
 
   left() {
-    if (this.cursor !== 0) {
-      this.cursor -= 1;
-      this.document[this.lastBufferIndex] = this.document[this.cursor];
-      this.document[this.cursor] = VOID;
-      this.lastBufferIndex -= 1;
+    if (this.gapLeft !== 0) {
+      this.gapLeft -= 1;
+      this.gapRight -= 1;
+      this.document[this.gapRight + 1] = this.document[this.gapLeft];
+      this.document[this.gapLeft] = VOID;
     }
   }
 
   right() {
-    if (this.lastBufferIndex !== this.document.length - 1) {
-      this.lastBufferIndex += 1;
-      this.document[this.cursor] = this.document[this.lastBufferIndex];
-      this.document[this.lastBufferIndex] = VOID;
-      this.cursor += 1;
+    if (this.gapRight !== this.document.length - 1) {
+      this.gapLeft += 1;
+      this.gapRight += 1;
+      this.document[this.gapLeft - 1] = this.document[this.gapRight];
+      this.document[this.gapRight] = VOID;
+    }
+  }
+
+  move(position) {
+    this.document = [
+      ..._.slice(this.document, 0, this.gapLeft),
+      ..._.slice(this.document, this.gapRight + 1),
+    ];
+    if (_.inRange(position, 0, this.document.length + 1)) {
+      this.grow(position);
     }
   }
 
   insert(char) {
-    this.document[this.cursor] = char;
-    this.cursor += 1;
+    this.document[this.gapLeft] = char;
+    this.gapLeft += 1;
+    this.gapSize -= 1;
+    if (this.gapSize === 0) {
+      this.grow(this.gapLeft);
+    }
   }
 }
--- packages/vulcan/tests/vulcan.test.js
+++ packages/vulcan/tests/vulcan.test.js
@@ -5,8 +5,16 @@ describe('vulcan gap buffer', () => {
   it('should be tested', () => {
     const buffer = new GapBuffer();
     buffer.insert('h');
+    buffer.insert('e');
+    buffer.insert('l');
+    buffer.insert('l');
+    buffer.insert('o');
     buffer.left();
-    buffer.right();
+    buffer.left();
+    buffer.left();
+    buffer.left();
+    buffer.left();
+    buffer.move(5);
     console.log(...buffer.document);
   });
 });
